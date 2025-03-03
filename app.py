bb8c14acce357501da89a7d988cc2035238f877f
GerardoNolasco05 <nolasco.gerardo@gmail.com>
2025-03-03 09:55:19 +0100

    upload new files

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # Open the posts.json file in the 'data' folder
+    with open(os.path.join('data', 'posts.json')) as file:
+        blog_posts = json.load(file)  # Load the posts data from JSON file
 
+    # Pass the 'blog_posts' to the template
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0git .0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
